914df84c3e73ad8e3190860ac2a43ef4d824d379
Alfred Boateng <programmingwithalfred@gmail.com>
2023-03-28 11:13:34 +0100

    Authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :users, path: 'api/auth', path_names: {
+    sign_in: 'login',
+    sign_out: 'logout',
+    registration: 'signup'
+  },
+  controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
   
    root "homes#index"
 end
